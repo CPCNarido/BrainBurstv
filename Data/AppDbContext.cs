d41d65497110377ef87dcc015d2783084b32b8e5
TroyMichaelCamarillo <tcamarillo.a12345639@umak.edu.ph>
2024-11-25 19:19:02 +0800

    modified:   Data/AppDbContext.cs 	modified:   Models/Flashcard.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,5 +9,20 @@ namespace UsersApp.Data
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Flashcard> Flashcards { get; set; }
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the relationships and constraints
+            modelBuilder.Entity<Flashcard>()
+                .HasMany(f => f.Questions)
+                .WithOne()
+                .HasForeignKey(q => q.FlashcardId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- Models/Flashcard.cs
+++ Models/Flashcard.cs
@@ -4,6 +4,7 @@ namespace UsersApp.Models
 {
     public class Flashcard
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public List<Question> Questions { get; set; }
@@ -11,8 +12,10 @@ namespace UsersApp.Models
 
     public class Question
     {
+        public int Id { get; set; }
         public string QuestionText { get; set; }
         public string AnswerText { get; set; }
         public string Picture { get; set; }
+        public int FlashcardId { get; set; }
     }
 }
