d475e1eea4fa007cc514084342347bfb2afde598
CPCNarido <Cnarido.a12345598@umak.edu.ph>
2024-12-02 17:51:26 +0800

    s

--- ViewModels/QuizDetailsViewModel.cs
+++ ViewModels/QuizDetailsViewModel.cs
@@ -4,10 +4,10 @@ namespace UsersApp.ViewModels
 {
     public class QuizDetailsViewModel
     {
-        public Dictionary<int, string> Questions { get; set; }
-        public Dictionary<int, List<string>> Choices { get; set; }
-        public Dictionary<int, int> Timer { get; set; }
+        public Dictionary<int, string> Questions { get; set; } = new Dictionary<int, string>();
+        public Dictionary<int, List<string>> Choices { get; set; } = new Dictionary<int, List<string>>();
+        public Dictionary<int, int> Timer { get; set; } = new Dictionary<int, int>();
         public int QuizId { get; set; }
-        public Dictionary<int, int> CorrectAnswers { get; set; } // Ensure this property is included
+        public Dictionary<int, int> CorrectAnswers { get; set; } = new Dictionary<int, int>();
     }
 }
